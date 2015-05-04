9bb45b21494c3117dc494fe980f6360658ed61ae
Alexander <alex.v.belozerov@gmail.com>
2015-05-04 18:59:36 +0300

    update tree view

--- Engine/DTCDev.Client.Cars.Service.Engine/Controls/ViewModels/Settings/SettingsWorkDicViewModel.cs
+++ Engine/DTCDev.Client.Cars.Service.Engine/Controls/ViewModels/Settings/SettingsWorkDicViewModel.cs
@@ -66,7 +66,26 @@ namespace DTCDev.Client.Cars.Service.Engine.Controls.ViewModels.Settings
 
         void Instance_LoadWorkListComplete(object sender, EventArgs e)
         {
-            FilterWorks();
+            WorkTreeModel model = WorksTree.Where(p => p.id == -1).First();
+            foreach (var item in model.Items)
+            {
+                item.Items.Clear();
+                List<WorksInfoDataModel> temp = SpecificationDataStorage.Instance.WorkList.Where(p => p.id_Class == item.id).ToList();
+                foreach (var w in temp)
+                {
+                    item.Items.Add(new WorkTreeModel
+                    {
+                        Cost = w.Cost,
+                        id = w.id,
+                        id_Class = w.id_Class,
+                        idWork = w.idWork,
+                        Name = w.Name,
+                        NH = w.NH,
+                        NHD = w.NHD,
+                        WGUID = w.WGUID
+                    });
+                }
+            }
         }
 
         void Instance_LoadOtherWorkListComplete(object sender, EventArgs e)
