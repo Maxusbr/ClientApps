8505a9354aac046fcf0d436ccdd1f1c26a8bd6cd
Alexander <alex.v.belozerov@gmail.com>
2015-05-26 01:16:32 +0300

    CARS fix accelerometer error

---
+++ Engine/DTCDev.Client.Cars.Service.Engine/Controls/ViewModels/Settings/UserSettingsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DTCDev.Client.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTCDev.Client.Cars.Service.Engine.Controls.ViewModels.Settings
+{
+    public class UserSettingsViewModel : ViewModelBase
+    {
+
+    }
+}
