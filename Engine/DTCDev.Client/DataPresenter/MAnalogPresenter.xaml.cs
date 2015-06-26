d863e410283ae0f78d26257b2c77823ca266b84d
Alexander <alex.v.belozerov@gmail.com>
2015-06-26 10:25:57 +0300

    fix analog display errors

--- Engine/DTCDev.Client/DataPresenter/MAnalogPresenter.xaml.cs
+++ Engine/DTCDev.Client/DataPresenter/MAnalogPresenter.xaml.cs
@@ -27,7 +27,7 @@ namespace DTCDev.Client.DataPresenter
 
         private int _start = 0;
         private int _stop = 180;
-        private int _mn = 0;
+        private int _mn = 1;
         private string _milest = "";
 
         public int Start
@@ -56,7 +56,7 @@ namespace DTCDev.Client.DataPresenter
             set
             {
                 _milest = value;
-                if (_mn < 1)
+                if (_mn == 1)
                     txtData.Text = _milest;
                 else
                     txtData.Text = _milest + ", x" + _mn.ToString();
@@ -71,12 +71,13 @@ namespace DTCDev.Client.DataPresenter
 
         private void UpdateParams()
         {
-            if(Stop>999)
+            int stp = _stop;
+            if(stp>999)
             {
-                Stop = Stop / 1000;
+                stp = stp / 1000;
                 _mn = 1000;
             }
-            int total = _stop - _start;
+            int total = stp - _start;
             int step = total / 6;
             txt1.Text = _start.ToString();
             txt2.Text = (_start + step).ToString();
@@ -85,9 +86,8 @@ namespace DTCDev.Client.DataPresenter
             txt5.Text = (_start + step * 4).ToString();
             txt6.Text = (_start + step * 5).ToString();
             txt7.Text = (_start + step * 6).ToString();
-            _stop = _start + step * 6;
-            double totDouble = total;
-            _degreesPerStep = 270.0f / totDouble;
+            _stop = _start + step * 6*_mn;
+            _degreesPerStep = 270.0d / (double)(_stop - _start);
         }
 
         private int _lastVol = 0;
