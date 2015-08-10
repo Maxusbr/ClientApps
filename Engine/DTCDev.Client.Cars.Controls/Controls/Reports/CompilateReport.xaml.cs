858a6a65939d0ed90012254d7ceeb39df02a1d97
Alexander <alex.v.belozerov@gmail.com>
2015-08-10 12:48:13 +0300

    add comilate car report

---
+++ Engine/DTCDev.Client.Cars.Controls/Controls/Reports/CompilateReport.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DTCDev.Client.Cars.Controls.Controls.Reports
+{
+    /// <summary>
+    /// Interaction logic for CompilateReport.xaml
+    /// </summary>
+    public partial class CompilateReport : UserControl
+    {
+        public CompilateReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
